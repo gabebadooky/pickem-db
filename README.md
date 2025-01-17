218922578d07d0c0d953b2b7ea147f2b969c5cd5
gabebadooky <baduquig@gmail.com>
2025-01-16 23:02:15 -0700

    Create Tables WIP...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pickem_db
 MySQL Database scripts for creating Pickem DB
+
+
+## Approach
+1. Identify desired data elements from each source website and normalize data model to mitigate redundancy ([Data Normalization Draft Doc](https://docs.google.com/spreadsheets/d/12aBpKssCciR3sFBb1Mrp15PZSPBCHbsKBGePMRpX4PY/edit?usp=sharing))
+2. Create Entity Relationship Diagram for Pickem database ([CFB Pickem ERD](https://lucid.app/lucidchart/b23cbf7a-b9f9-4ce6-b310-3fb8cbcc6329/edit?viewport_loc=-1207%2C-1018%2C3577%2C2203%2C0_0&invitationId=inv_fb883cc0-8449-4625-9a2e-8be28ce6ef22))
---
+++ create_tables.sql
@@ -0,0 +1,102 @@
+/******************************
+File: create_tables.sql
+Last Update: 1/16/2025
+Description: This script defines the tables and constraints
+             for the PICKEM_DB database.
+******************************/
+
+
+CREATE TABLE SCORING (
+    PICK_WEIGHT     CHAR(1)         NOT NULL,
+    REWARD          TINYINT         NOT NULL,
+    PENALTY         TINYINT         NOT NULL
+
+    CONSTRAINT PK_SCORING PRIMARY KEY (PICK_WEIGHT)
+);
+
+CREATE TABLE USERS (
+    USER_ID         INT             NOT NULL    AUTO_INCREMENT,
+    USERNAME        VARCHAR(25)     NOT NULL,
+    PWDHASH         VARCHAR(25)     NOT NULL,
+    NOTIFICATION_PREF   CHAR(1)     NULL,
+    EMAIL_ADDRESS   VARCHAR(75)     NULL,
+    PHONE           VARCHAR(10)     NULL,
+    
+    CONSTRAINT PK_USERS PRIMARY KEY (USER_ID)
+);
+
+CREATE TABLE PICKS (
+    PICK_ID         VARCHAR(50)     NOT NULL,
+    USER_ID         INT             NOT NULL,
+    GAME_ID         VARCHAR(25)     NOT NULL,
+    TEAM_PICKED     VARCHAR(25)     NULL,
+    PICK_WEIGHT     CHAR(1)         NULL,
+
+    CONSTRAINT PK_PICKS PRIMARY KEY (PICK_ID),
+    CONSTRAINT FK_USERPICK 
+        FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+    CONSTRAINT FK_PICKSCORE 
+        FOREIGN KEY (PICK_WEIGHT) REFERENCES SCORING(PICK_WEIGHT)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE GAME_BOX_SCORES (
+    GAME_ID         VARCHAR(25)     NOT NULL,
+    TEAM_ID         VARCHAR(25)     NOT NULL,
+    Q1_SCORE        SMALLINT        NULL,
+    Q2_SCORE        SMALLINT        NULL,
+    Q3_SCORE        SMALLINT        NULL,
+    Q4_SCORE        SMALLINT        NULL,
+    OVERTIME        SMALLINT        NULL,
+    TOTAL           SMALLINT        NULL,
+    GAME_FINISHED   TINYINT(1)      NOT NULL
+
+    CONSTRAINT PK_GAME_BOX_SCORES PRIMARY KEY (GAME_ID, TEAM_ID)
+);
+
+CREATE TABLE LOCATIONS (
+    STADIUM         VARCHAR(50)     NOT NULL,
+    CITY            VARCHAR(50)     NOT NULL,
+    STATE           VARCHAR(20)     NULL,
+    LATITUDE        DECIMAL(19,16)  NULL,
+    LONGITUDE       DECIMAL(19,16)  NULL,
+
+    CONSTRAINT PK_LOCATIONS PRIMARY KEY (STADIUM, CITY)
+);
+
+CREATE TABLE TEAM_RECORD (
+    TEAM_ID         VARCHAR(25)     NOT NULL,
+    RECORD_TYPE     VARCHAR(10)     NOT NULL,
+    WINS            TINYINT         NULL,
+    LOSSES          TINYINT         NULL,
+    TIES            TINYINT         NULL,
+
+    CONSTRAINT PK_TEAM_RECORD PRIMARY KEY (TEAM_ID, RECORD_TYPE)
+);
+
+
+/*** GAMES ***
+-- PRIMARY KEY: 
+CREATE TABLE GAMES (
+    GAME_ID         VARCHAR(25)     NOT NULL    PRIMARY KEY,
+    LEAGUE          VARCHAR(5)      NOT NULL,
+    WEEK            TINYINT         NOT NULL,
+    CBS_CODE        VARCHAR(50)     NOT NULL,
+    ESPN_CODE       VARCHAR(50)     NOT NULL,
+    FOX_CODE        VARCHAR(50)     NOT NULL,
+    VEGAS_CODE      VARCHAR(50)     NOT NULL,
+    AWAY_TEAM_ID    VARCHAR(25)     NOT NULL,
+    HOME_TEAM_ID    VARCHAR(25)     NOT NULL,
+    DATE            DATE            NULL,
+    TIME            TIME            NULL,
+    TV_COVERAGE     VARCHAR(25)     NULL,
+    STADIUM         VARCHAR(50)     NULL,
+    CITY            VARCHAR(50)     NULL,
+    STATE           VARCHAR(50)     NULL
+
+    -- FOREIGN KEY (AWAY_TEAM_ID) REFERENCES TEAMS(TEAM_ID)
+    -- FOREIGN KEY (HOME_TEAM_ID) REFERENCES TEAMS(TEAM_ID)
+); */
