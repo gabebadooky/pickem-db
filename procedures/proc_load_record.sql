dadad0c6c0ee6afa4e5ee2bab0b149546df61030
gbaduqui <baduquig@gmail.com>
2025-02-26 12:33:36 -0700

    Develop PROC_LOAD_RECORD procedure

---
+++ procedures/proc_load_record.sql
@@ -0,0 +1,44 @@
+/******************************
+File: proc_load_records.sql
+Last Update: 2/26/2025
+Description: This script inserts a new records record if one does not
+				already exist or updates the existing one
+******************************/
+
+USE PICKEM_DB;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS PROC_LOAD_RECORDS //
+
+CREATE PROCEDURE PROC_LOAD_RECORDS (
+	IN IN_TEAM_ID		VARCHAR(25),
+	IN IN_RECORD_TYPE	VARCHAR(10),
+	IN IN_WINS			TINYINT,
+    IN IN_LOSSES		TINYINT,
+    IN IN_TIES			TINYINT
+)
+BEGIN
+	
+    IF (SELECT COUNT(*) FROM RECORDS WHERE TEAM_ID = IN_TEAM_STATS AND RECORD_TYPE = IN_RECORD_TYPE) = 0 THEN
+		
+        # Insert new row 
+        INSERT INTO RECORDS (TEAM_ID, RECORD_TYPE, WINS, LOSSES, TIES)
+			VALUES (IN_TEAM_ID, IN_RECORD_TYPE, IN_WINS, IN_LOSSES, IN_TIES);
+	
+    ELSE
+        
+        # Update existing row
+        UPDATE RECORDS 
+			SET 
+				WINS = IN_WINS,
+				LOSSES = IN_LOSSES,
+                TIES = IN_TIES
+			WHERE TEAM_ID = IN_TEAM_ID
+				AND RECORD_TYPE = IN_RECORD_TYPE;
+        
+	END IF;
+
+END //
+
+DELIMITER ;
