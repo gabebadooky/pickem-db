60146931bc7b95e836578efb721839e5ece6daaa
gbaduqui <baduquig@gmail.com>
2025-02-26 12:32:44 -0700

    Develop PROC_LOAD_LOCATION procedure

---
+++ procedures/proc_load_location.sql
@@ -0,0 +1,44 @@
+/******************************
+File: proc_load_locations.sql
+Last Update: 2/26/2025
+Description: This script inserts a new locations record if one does not
+				already exist or updates the existing one
+******************************/
+
+USE PICKEM_DB;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS PROC_LOAD_LOCATIONS //
+
+CREATE PROCEDURE PROC_LOAD_LOCATIONS (
+	IN  IN_STADIUM      VARCHAR(50),
+	IN  IN_CITY         VARCHAR(50),
+	IN  IN_STATE        VARCHAR(20),
+	IN  IN_LATITUDE     DECIMAL(19,16),
+	IN  IN_LONGITUDE	DECIMAL(19,16) 
+)
+BEGIN
+	
+    IF (SELECT COUNT(*) FROM LOCATIONS WHERE STADIUM = IN_STADIUM AND CITY = IN_CITY) = 0 THEN
+		
+        # Insert new row 
+        INSERT INTO LOCATIONS (STADIUM, CITY, STATE, LATITUDE, LONGITUDE)
+			VALUES (IN_STADIUM, IN_CITY, IN_STATE, IN_LATITUDE, IN_LONGITUDE);
+	
+    ELSE
+        
+        # Update existing row
+        UPDATE LOCATIONS 
+			SET 
+				STATE = IN_STATE,
+				LATITUDE = IN_LATITUDE,
+                LONGITUDE = IN_LONGITUDE
+			WHERE STADIUM = IN_STADIUM
+				AND CITY = IN_CITY;
+        
+	END IF;
+
+END //
+
+DELIMITER ;
