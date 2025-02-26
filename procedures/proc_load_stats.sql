b4b12b5e3a4f17a7adb52c8928c57d187d9077a6
gbaduqui <baduquig@gmail.com>
2025-02-25 18:26:02 -0700

    Develop PROC_LOAD_ODDS and PROC_LOAD_STATS procedures

---
+++ procedures/proc_load_stats.sql
@@ -0,0 +1,39 @@
+/******************************
+File: proc_load_stats.sql
+Last Update: 2/25/2025
+Description: This script inserts a new stats record if one does not
+				already exist or updates the existing one
+******************************/
+
+USE PICKEM_DB;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS PROC_LOAD_STATS //
+
+CREATE PROCEDURE PROC_LOAD_STATS (
+	IN IN_TEAM_ID	VARCHAR(25),
+	IN IN_TYPE      VARCHAR(50),
+	IN IN_VALUE     VARCHAR(25)
+)
+BEGIN
+	
+    IF (SELECT COUNT(*) FROM TEAM_STATS WHERE TEAM_ID = IN_TEAM_STATS AND TYPE = IN_TYPE) = 0 THEN
+		
+        # Insert new row 
+        INSERT INTO TEAM_STATS (TEAM_ID, TYPE, VALUE)
+			VALUES (IN_TEAM_ID, IN_TYPE, IN_VALUE);
+	
+    ELSE
+        
+        # Update existing row
+        UPDATE TEAM_STATS 
+			SET VALUE = IN_VALUE
+			WHERE TEAM_ID = IN_TEAM_ID
+				AND SOURCE = IN_TYPE;
+        
+	END IF;
+
+END //
+
+DELIMITER ;
