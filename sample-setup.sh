2398f9d543dd30f1fbbbb8acfcb3f11900432513
gbaduqui <baduquig@gmail.com>
2025-04-29 18:59:59 -0700

    fix directory to sql files

--- sample-setup.sh
+++ sample-setup.sh
@@ -1,5 +1,6 @@
 # Copy this file and rename as setup.sh
 # Replace my-password, path/to/directory, username, and database_name for PICKEM_DB
+# Ensure `setup.sh` stays included in the .gitignore file
 
 # Connection parametersc
 export MYSQL_PWD=""
@@ -7,7 +8,7 @@ username="my-username"
 host="localhost"
 port=3306
 database="my_db"
-directory=./*.sql
+directory=./procedures/*.sql
 
 
 mysql -u $username -e "CREATE DATABASE IF NOT EXISTS $database"
